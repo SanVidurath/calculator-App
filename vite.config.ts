590a78b3a7df69a0ec9f7d806112861d484f4b82
Sandeepa <sanvidurath98@gmail.com>
2024-02-14 11:24:44 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import VueImages from "vite-plugin-vue-images";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VueImages(), // Add the ViteImagesPlugin here
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // set your desired limit in kilobytes
+  },
+});
